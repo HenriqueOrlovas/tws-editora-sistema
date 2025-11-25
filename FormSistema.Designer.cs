690d91c3e1a0207fdde30495925003da1559ca9d
luiza <luiza.alves19@etec.sp.gov.br>
2025-11-25 16:46:44 -0300

    TwsEditora.0.2

--- FormSistema.Designer.cs
+++ FormSistema.Designer.cs
@@ -245,7 +245,7 @@ namespace SeuProjeto
             // pictureBox1
             // 
             this.pictureBox1.Image = global::tws_sistema.Properties.Resources.slogan_Tws1;
-            this.pictureBox1.Location = new System.Drawing.Point(1014, 18);
+            this.pictureBox1.Location = new System.Drawing.Point(1006, 18);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(74, 76);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
