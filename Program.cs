6fc8537601294f3fb9befc7805103fb921657600
henrique <henrique.orlovas@etec.sp.gov.br>
2025-11-27 22:07:01 -0300

    Adição da Tela de Login

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace SeuProjeto
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormSistema());
+            Application.Run(new Login());
         }
     }
 }
